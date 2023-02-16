a4f5beca7a5d55a13460a9215c7f286d07181abc
DESKTOP-FD066BN\Jeoffy <jeoffy_hipolito@yahoo.com>
2023-02-16 18:38:40 +1300

    Added forms and styled-component

--- src/components/PokeNameComponent.js
+++ src/components/PokeNameComponent.js
@@ -4,12 +4,30 @@ import {useSelector} from "react-redux";
 import { useCallback  } from 'react';
 import { CarouselComponent } from './CarouselComponent';
 import BadgeComponent from './BadgeComponent';
+import { useForm } from "react-hook-form";
+import styled from 'styled-components';
+
+const FormStyle = styled.div`
+    border: 1px solid #e5e5e5;
+    padding: 1rem;
+    margin-top: 2rem;
+    margin-bottom:2rem;
+    display: flex;
+    flex-direction:   column;
+    input { 
+        width: 100%;
+        margin:0.5rem 0;
+        padding: 1rem;
+    }
+`;
 
 const PokeNameComponent = () => {
 
     const navigate = useNavigate();
     const params = useParams();
     const pokemonDatas = useSelector((state)=>state)
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data); // Save to DB
 
     const getPokemonSelector = useCallback(()=>{
             if(pokemonDatas && params) {
@@ -37,7 +55,16 @@ const PokeNameComponent = () => {
                 }
                 
                 <p>Height: {eachDetails?.height}</p>
-                <p>Weight: {eachDetails?.weight}</p>
+                <p>Weight: {eachDetails?.weight}</p> 
+                <FormStyle>
+                    <form onSubmit={handleSubmit(onSubmit)}> 
+                        <input defaultValue={params.name} {...register("pokeName", { required: true })} /> 
+                        {/* errors will return when field validation fails  */}
+                        {errors.pokeName && <span>This field is required</span>}
+                        
+                        <input type="submit" />
+                    </form>
+                </FormStyle> 
 
                 <p><span onClick={()=>navigate('/pokemons')} className={'btn btn-primary'}>Go Back</span></p>
             </div>
